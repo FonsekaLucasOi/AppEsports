85b7e5831ceb87effb3232bd816d6c10ad0c1221
FonsekaLucas <fonsekalucas@gmail.com>
2018-11-11 01:37:22 -0200

    Criação da estrutura raiz do projeto
    Criação da estrutura raiz com as principais dependências e configurações para o projeto.

---
+++ app_esports/src/main/java/br/com/appesports/app_esports/AppEsportsApplication.java
@@ -0,0 +1,12 @@
+package br.com.appesports.app_esports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEsportsApplication.class, args);
+	}
+}
